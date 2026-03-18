dd4f6760005d71b7ebabe167b744221bcc188641
Sourabh SIngh <singhsourav090305@gmail.com>
2026-03-19 02:27:10 +0530

    change

--- server/config/email.js
+++ server/config/email.js
@@ -4,7 +4,7 @@ const createTransporter = () => {
   return nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: parseInt(process.env.SMTP_PORT, 10),
-    secure: process.env.SMTP_PORT === '465',
+    secure: process.env.SMTP_PORT === '587',
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS,
